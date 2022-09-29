a8bda1ab892df0a3d3c53b9d8e725f0e41d168c3
krahman <koliur.rahman@my.liu.edu>
2022-09-29 09:51:35 -0400

    fixed fetching with http instead of https

--- src/components/TimeTable.js
+++ src/components/TimeTable.js
@@ -20,7 +20,7 @@ export default function TimeTable(props) {
 
     React.useEffect(() => {
         if (country && state && city) {
-            fetch('http://api.aladhan.com/v1/calendarByCity?' + new URLSearchParams({
+            fetch('https://api.aladhan.com/v1/calendarByCity?' + new URLSearchParams({
                 city: city,
                 country: country,
                 state: state,
